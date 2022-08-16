18290fc9782e5fde2dc3df35714431d66256af87
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-16 18:36:13 +0530

    arithmetic operators

---
+++ operators.js
@@ -0,0 +1,42 @@
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+
+var a = 20;
+var b = 10;
+var c = a - b;
+console.log(c);
+
+
+var a = 10;
+var b = 20;
+var c = a * b;
+console.log(c);
+
+
+var a = 5;
+var b = 6;
+var c = a**b;
+console.log(c);
+
+
+var a = 10;
+var b = 20;
+var c = a % b;
+console.log(c);
+
+
+var a = 10;
+var b = 20;
+var c = a/b;
+console.log(c);
+
+
+var a = 10;
+var b = 20;
+ a++;
+ b--;
+console.log(a+b);
+
